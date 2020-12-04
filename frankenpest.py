f6b89c647b42987b35638af8d3e7f687d381a980
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2020-12-04 19:32:39 +0100

    Update urls to now changed addresses

--- frankenpest.py
+++ frankenpest.py
@@ -5,30 +5,33 @@ from lib.convert import Convert
 from lib.extra import fp_arg_premium
 from lib.generate import Generate
 
-BASE_URL = 'http://www.frankenpost.de'
+BASE_URL = 'https://www.frankenpost.de'
 FEED_INPUT = {
     name: ''.join([
-        BASE_URL, '/storage/rss/rss/fp/', name, '.xml'
+        BASE_URL, '/', name, '.rss.feed'
     ]) for name in (
-        'homepage',
-        'nachrichten_fichtelgebirge',
-        'nachrichten_hofrehau',
-        'nachrichten_kulmbach',
-        'nachrichten_kultur',
-        'nachrichten_muenchberg',
-        'nachrichten_naila',
-        'nachrichten_regional',
-        'nachrichten_selb',
-        'nachrichten_wirtschaft',
-        'sport_verselb',
         'topmeldung',
+        'auf-einen-blick',
+        'region',
+        'region/hof',
+        'region/rehau',
+        # 'region/naila',
+        # 'region/muenchberg',
+        'region/fichtelgebirge',
+        'region/wunsiedel',
+        'region/marktredwitz',
+        'region/selb',
+        'region/arzberg',
+        # 'region/kulmbach',
+        'sport/ver_selb',
+        'sport/bayern_hof',
     )
 }
 
 
 def main():
     args = arguments(__file__, FEED_INPUT, extra=fp_arg_premium)
-    conv = Convert(args, FEED_INPUT, encoding='ISO-8859-1')
+    conv = Convert(args, FEED_INPUT, encoding='UTF-8')
     return 0 if Generate(conv)(BASE_URL) else 1
 
 
