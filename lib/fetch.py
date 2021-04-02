b6eb9d0c613003064d16f60ba99b40978b232af9
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2021-04-02 12:50:35 +0200

    Local newspaper delivers trash in the feeds - try to fix that

--- lib/fetch.py
+++ lib/fetch.py
@@ -1,6 +1,8 @@
 from html import unescape
+from urllib.error import HTTPError
 from urllib.request import urlopen
 from xml.dom.minidom import parseString
+from xml.parsers.expat import ExpatError  # pylint: disable=no-name-in-module
 
 from lib.extra import FIELDS
 
@@ -15,9 +17,15 @@ class Fetch:
     @property
     def doc(self):
         if self._doc is None:
-            with urlopen(self.url) as resp:
-                doc = resp.read().decode(self.encoding)
-                self._doc = parseString(doc)
+            try:
+                with urlopen(self.url) as resp:
+                    doc = resp.read().decode(self.encoding)
+                    if doc:
+                        self._doc = parseString(doc)
+
+            except (HTTPError, ExpatError):
+                return None
+
         return self._doc
 
     @staticmethod
@@ -32,6 +40,9 @@ class Fetch:
                     yield (elem.nodeName, content)
 
     def __call__(self):
+        if self.doc is None:
+            return
+
         for item in self.doc.getElementsByTagName('item'):
             result = {'origin': self.name}
             result.update(self.entry(item))
